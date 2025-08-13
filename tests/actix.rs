f273a6ffe61b41f3e9c27885e64c3c9119be2c0b
ppoliani <ppoliani@gmail.com>
2025-08-13 19:34:58 +0300

    feat: add additional config to the session middleware

--- tests/actix.rs
+++ tests/actix.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests {
-  use deadpool_redis::{Config, Runtime};
+  use actix_session::config::{BrowserSession, SessionLifecycle, TtlExtensionPolicy};
+  use actix_web::cookie::time::Duration;
+use deadpool_redis::{Config, Runtime};
   use actix_session::storage::RedisSessionStore;
   use actix_session::SessionMiddleware;
   use actix_web::cookie::{Key, SameSite};
@@ -12,7 +14,6 @@ mod tests {
   use actix_idempotent::{IdempotentFactory, IdempotentOptions};
   use bytes::Bytes;
   use std::sync::atomic::{AtomicU64, Ordering};
-  use std::time::Duration;
 
   static COUNTER: AtomicU64 = AtomicU64::new(0);
 
@@ -22,19 +23,24 @@ mod tests {
   }
 
   async fn create_test_app() -> impl Service<Request, Response = ServiceResponse, Error = Error> {
-    let conn_string = format!("redis://:{}@{}:{}", "Lma5LVU8lMcDRAFwKMLmcUuiIQ+uXaEZIm2eahgr", "127.0.0.1", "6379");
+    let conn_string = format!("redis://:{}@{}:{}", "password", "127.0.0.1", "6379");
     let config = Config::from_url(conn_string);
     let pool = config.create_pool(Some(Runtime::Tokio1)).unwrap();
     let redis_store = RedisSessionStore::new_pooled(pool).await.unwrap();
 
     let idempotent_factory = IdempotentFactory::new(IdempotentOptions::default().expire_after(3));
     let secret_key = Key::generate();
-
     let app = test::init_service(
       App::new()
         // Add session management to your application using Redis for session state storage
         .wrap(
           SessionMiddleware::builder(redis_store.clone(), secret_key.clone())
+            .cookie_name("session".to_string())
+            .session_lifecycle(SessionLifecycle::BrowserSession(
+              BrowserSession::default()
+              .state_ttl_extension_policy(TtlExtensionPolicy::OnEveryRequest)
+              .state_ttl(Duration::seconds(10))
+            ))
             // allow the cookie to be accessed from javascript
             .cookie_http_only(false)
             // allow the cookie only from the current domain
@@ -57,12 +63,16 @@ mod tests {
       .send_request(&mut app).await;
     assert!(response1.status().is_success(), "Something went wrong");
 
+    for h in response1.headers() {
+      println!("{} => {:?}", h.0, h.1)
+    }
+
     // First request should increment counter
     // Extract the session cookie from the first response
     let session_cookie = response1
       .headers()
       .get_all("set-cookie")
-      .find(|&cookie| cookie.to_str().unwrap().starts_with("id="))
+      .find(|&cookie| cookie.to_str().unwrap().starts_with("session="))
       .cloned()
       .expect("Session cookie not found");
 
@@ -80,7 +90,7 @@ mod tests {
     assert_eq!(&body2[..], b"Response #0");
 
     // Wait for cache to expire
-    tokio::time::sleep(Duration::from_secs(3)).await;
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
 
     // Third request should increment counter again - still include the session cookie
     let response3 = TestRequest::get()
